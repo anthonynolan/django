06b49f464030f87efea243877a095a709c3e88c3
Anthony Nolan <anthonynolan@gmail.com>
2023-02-03 14:03:29 +0000

    Includes both front and back end

--- goals/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Goal
-
-
-class GoalSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Goal
-        fields = ("id", "name", "status")
